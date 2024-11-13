f40cc4c097034370ec21fed55087179bc341df8d
Patricia <125956911+Patricia-Mekan@users.noreply.github.com>
2024-11-14 00:58:40 +0300

    New Pages, Contact form update & Text Update

--- script.js
+++ script.js
@@ -29,3 +29,20 @@ function openmenu(){
 function closemenu(){
     sidemenu.style.right = "-200px";
 }
+
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbymJYXIzcIbUKri5F8eOdGwe3T4Xecp3dKRvp1pqvJtCVVLt9OtUtjkOkdi2M02S15o/exec'
+  const form = document.forms['submit-to-google-sheet']
+  const msg = document.getElementById("msg")
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML = "Message Sent Successfully"
+        setTimeout(function(){
+            msg.innerHTML = ""
+        }, 5000)
+        form.reset()
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
